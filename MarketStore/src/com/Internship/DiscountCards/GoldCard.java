74321fcffdc4a5d8c632edf03579d9e057c337ab
nenadmit <58050465+nenadmit@users.noreply.github.com>
2020-02-11 23:10:21 +0100

    Update GoldCard.java

--- MarketStore/src/com/Internship/DiscountCards/GoldCard.java
+++ MarketStore/src/com/Internship/DiscountCards/GoldCard.java
@@ -10,10 +10,11 @@ public class GoldCard implements DiscountCard {
 
     public GoldCard(double turnover, double purchaseValue){
 
-        if (turnover < 0 || purchaseValue <0){
+        if (turnover < 0 )
             turnover = 0;
+        
+        if (purchaseValue<0)
             purchaseValue=0;
-        }
 
         this.turnover = turnover;
         this.purchaseValue = purchaseValue;
