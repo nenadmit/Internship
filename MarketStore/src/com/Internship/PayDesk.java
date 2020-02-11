31b4b1fb20c654cb83fbc8efb716a1d55f005333
nenadmit <58050465+nenadmit@users.noreply.github.com>
2020-02-12 00:26:33 +0100

    Update PayDesk.java

--- MarketStore/src/com/Internship/PayDesk.java
+++ MarketStore/src/com/Internship/PayDesk.java
@@ -1,25 +1,19 @@
 package com.Internship;
 
+import com.Internship.Calculator.Calculator;
 import com.Internship.DiscountCards.DiscountCard;
 
 public class PayDesk {
 
     public static void print(DiscountCard discountCard){
 
+        Calculator calculator = new Calculator(discountCard);
+
         System.out.println("Purchase value: $"+ discountCard.getPurchaseValue() );
         System.out.println("Discount rate: "+ discountCard.getDiscountRate()+"%" );
-        System.out.println("Discount: $"+ calculateDiscount(discountCard) );
-        System.out.println("Total: $"+ calculateTotal(discountCard));
+        System.out.println("Discount: $"+ calculator.calculateDiscount() );
+        System.out.println("Total: $"+ calculator.calculateTotal());
 
-    }
-
-    private static double calculateDiscount(DiscountCard discountCard){
-        return discountCard.getPurchaseValue() * discountCard.getDiscountRate()/100;
-
-    }
-
-    private static double calculateTotal(DiscountCard discountCard){
-        return discountCard.getPurchaseValue() - calculateDiscount(discountCard);
     }
 
 }
